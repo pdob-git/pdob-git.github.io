4068a91cef281ca880c8f888a9ed0050c9eea3e7
Pablo <75912982+pdob-git@users.noreply.github.com>
2024-01-12 00:45:07 +0100

    Experiments to add button to hide recomendation
    Added changes to enable hidding
    ----------- NOT WORKING --------------
    Only button to hide was possible to be added

--- src/js/main/recommendation.js
+++ src/js/main/recommendation.js
@@ -1,40 +1,64 @@
 (function () {
-    'use strict';
+  "use strict";
 
-    var recommendation = document.querySelector('.recommendation');
-    var isVisible = false;
+  var recommendation = document.querySelector(".recommendation");
+  var buttonHide = recommendation.getElementsByTagName("button").querySelector(".hidding");
+  var isVisible = false;
 
-    if (recommendation) {
-        // Back to top button
-        var goBackToTop = recommendation.querySelector('.message button');
-        goBackToTop.addEventListener('click', function () {
-            scrollToTop();
-            return false;
-        });
+  if (recommendation) {
+    // Back to top button
+    var goBackToTop = recommendation.querySelector(".message button");
+    goBackToTop.addEventListener("click", function () {
+      scrollToTop();
+      return false;
+    });
 
-        // Hide
-        document.addEventListener('stillReading', function (elem) {
-            if (isVisible) {
-                recommendation.style.bottom = '-100%';
-                isVisible = false;
-            }
-        }, false);
-
-        // Show
-        document.addEventListener('finishedReading', function (elem) {
-            if (!isVisible) {
-                recommendation.style.bottom = '0%';
-                isVisible = true;
-            }
-        }, false);
-    }
-
-    var timeOut;
-    function scrollToTop() {
-        if (document.body.scrollTop != 0 || document.documentElement.scrollTop != 0) {
-            window.scrollBy(0, -50);
-            timeOut = setTimeout(scrollToTop, 10);
+    // Hide
+    document.addEventListener(
+      "stillReading",
+      function (elem) {
+        if (isVisible) {
+          recommendation.style.bottom = "-100%";
+          isVisible = false;
         }
-        else clearTimeout(timeOut);
-    }
+      },
+      false
+    );
+
+    // Hide button
+   
+    // buttonHide.addEventListener("click", function () {
+    //     if (isVisible && recommendation.style.display !== 'none') {
+    //         recommendation.style.display = 'none';
+    //         isVisible = false;
+    //     }
+    //     console.log("Hide button clicked");
+    //   });
+
+    // Show
+    document.addEventListener(
+      "finishedReading",
+      function (elem) {
+        if (!isVisible) {
+          recommendation.style.bottom = "0%";
+          // recommendation.style.display = block;
+          isVisible = true;
+        }
+      },
+      false
+    );
+
+    
+  }
+
+  var timeOut;
+  function scrollToTop() {
+    if (
+      document.body.scrollTop != 0 ||
+      document.documentElement.scrollTop != 0
+    ) {
+      window.scrollBy(0, -50);
+      timeOut = setTimeout(scrollToTop, 10);
+    } else clearTimeout(timeOut);
+  }
 })();
